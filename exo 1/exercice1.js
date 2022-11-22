2e623a32b30d932f56118439ebc59dc4f41e699c
azxki <cococoursan@gmail.com>
2022-11-22 11:49:04 +0100

    EXO

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,4 +1,4 @@
-
+document.getElementById("id-name").innerHTML = "Corentin Coursan";
 
 
 
--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -1,4 +1,9 @@
+title = document.getElementsByTagName('h1');
 
+for (title = 0; title < 3; title++) {
+
+    title.innerHTML = "Bonjour, je suis";
+}
 
 
 
